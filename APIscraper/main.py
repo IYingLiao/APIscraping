f336488ab517b4a49ab0572c4258e5332764a21b
IYingLiao <ml5523@ic.ac.uk>
2026-01-13 22:26:14 +0000

    Update comment in main.py

--- APIscraper/main.py
+++ APIscraper/main.py
@@ -1,4 +1,4 @@
-# Reference - this script involves the use of Cursor AI https://cursor.com
+# Reference 2 - this script involves the use of Cursor AI https://cursor.com
 
 """Main orchestrator for PubMed article fetching with entity extraction."""
 
