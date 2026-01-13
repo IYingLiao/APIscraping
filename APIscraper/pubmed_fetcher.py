94a7eb7401ee98ff82bd7e0b0a79b99f94c98687
IYingLiao <ml5523@ic.ac.uk>
2026-01-13 22:26:00 +0000

    Update comment in pubmed_fetcher.py

--- APIscraper/pubmed_fetcher.py
+++ APIscraper/pubmed_fetcher.py
@@ -1,4 +1,4 @@
-# Reference - this script involves the use of Cursor AI https://cursor.com
+# Reference 2 - this script involves the use of Cursor AI https://cursor.com
 
 """
 Lightweight PubMed API fetcher to produce the same basic text layout this
