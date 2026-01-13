2f8106e410863cf4296b91014000228864061236
IYingLiao <ml5523@ic.ac.uk>
2026-01-13 22:25:39 +0000

    Update comment in pubtator_entities.py

--- APIscraper/pubtator_entities.py
+++ APIscraper/pubtator_entities.py
@@ -1,4 +1,4 @@
-# Reference - this script involves the use of Cursor AI https://cursor.com
+# Reference 2 - this script involves the use of Cursor AI https://cursor.com
 
 """
 Fetch PubTator3 entity annotations for a PubMed PMID.
